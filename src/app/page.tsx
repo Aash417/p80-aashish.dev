9d2258a7b312d61ce4b6c4936794fc7f88ec422a
aashish_k <aashishkathat00@gmail.com>
2025-04-16 20:08:16 +0530

    added all projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,148 +1,209 @@
-
 export default function Home() {
-  return (
-    <div className="min-h-screen flex flex-col items-center px-3 font-sans pb-10"
-      style={{
-        // @ts-expect-error custom css variable
-        "--p-container": "0.75rem"
-      }}
-    >
-      <div className="w-full max-w-screen-sm min-h-screen"
-        style={{
-          // @ts-expect-error custom css variable
-          "--w-content": "640px"
-        }}
+   return (
+      <div
+         className="min-h-screen flex flex-col items-center px-3 font-sans pb-10"
+         style={{
+      // @ts-expect-error custom css variable
+            '--p-container': '0.75rem',
+         }}
       >
-        <header className="pt-28 tracking-tight">
-          <div className="text-4xl tracking-tighter">alfon</div>
-          <div className="font-light text-foreground2 font-mono flex gap-x-4 flex-wrap">
-            <div className="w-full sm:w-auto">
-              alfonsus ardani
-            </div>
-            <div className="hidden sm:block">
-              -
-            </div>
-            {
-              [
-                { url: "https://x.com/alfonsusac", description: "X" },
-                { url: "https://github.com/alfonsusac", description: "github" },
-                { url: "https://www.linkedin.com/in/alfonsusac/", description: "linkedin" },
-                { url: "https://bsky.app/profile/alfonsus.bsky.social", description: "bsky" },
-                { url: "https://discord.gg/c8MYbXtfvG", description: "discord" },
-              ].map((a, i) => {
-                return (
-                  <div key={i} className="">
-                    <a href={a.url} target="_blank" className="text-foreground2 hover:text-foreground">{a.description}</a>{' '}
+         <div
+            className="w-full max-w-(--breakpoint-sm) min-h-screen"
+            style={{
+         // @ts-expect-error custom css variable
+               '--w-content': '640px',
+            }}
+         >
+            <header className="pt-28 tracking-tight">
+               <div className="text-4xl tracking-tighter">aashish</div>
+               <div className="font-light text-foreground2 font-mono flex gap-x-4 flex-wrap">
+                  <div className="w-full sm:w-auto">aashish kathait</div>
+                  <div className="hidden sm:block">-</div>
+                  {[
+                     { url: 'https://x.com/aashish_kathait', description: 'X' },
+                     {
+                        url: 'https://github.com/Aash417',
+                        description: 'github',
+                     },
+                  ].map((a, i) => {
+                     return (
+                        <div key={i + 1} className="">
+                           <a
+                              href={a.url}
+                              target="_blank"
+                              className="text-foreground2 hover:text-foreground"
+                           >
+                              {a.description}
+                           </a>{' '}
+                        </div>
+                     );
+                  })}
+               </div>
+            </header>
+
+            <section className="pt-4">
+               <div className="text-foreground font-mono text-sm">
+                  i am a fullstack developer who builds
+                  end-to-end web apps, from backend logic to polished UIs.
+               </div>
+            </section>
+
+            <section className="pt-12">
+               <div className="font-mono text-lg tracking-tight text-foreground2 font-light pb-4">
+                  fullstack projects
+               </div>
+               {[
+                  [
+                     'formaker.com',
+                     'https://formakercom.vercel.app/',
+                     'Generate dynamic forms & collect responses from anyone',
+                  ],
+                  [
+                     'videoLib.com',
+                     'https://vlibcom.vercel.app/',
+                     'Video-sharing platform  upload, watch, and engage with content',
+                  ],
+                  [
+                     'schedule.com',
+                     'https://schedule-com-phi.vercel.app/',
+                     'Project management tool, invite team, track tasks, and manage projects',
+                  ],
+                  [
+                     'chatify.com',
+                     'https://chatify-com.vercel.app/',
+                     'Messaging platform for teams, communication through channels, DM, threads',
+                  ],
+                  [
+                     'cobracase.com',
+                     'https://p65-casecobra-next.vercel.app/',
+                     'Saas platform for your image on a custom phone case',
+                  ],
+                  [
+                     'blogged.com',
+                     'https://p62-blog-medium.vercel.app',
+                     'Publishing platform, share stories, ideas, and insights',
+                  ],
+                  [
+                     'tour-booker.com',
+                     'https://tour-booker.onrender.com/',
+                     'Platform for travel destinations, view details, photos, and book tours',
+                  ],
+                  [
+                     'wild-owasis-admin.com',
+                     'https://wild-owasis-admin.netlify.app',
+                     'Admin dashboard for a hotel booking platform',
+                  ],
+               ].map((a, i) => {
+                  return (
+                     <div key={i + 1}>
+                        <a
+                           href={a[1]}
+                           target="_blank"
+                           className="text-foreground3 hover:text-foreground"
+                        >
+                           {a[0]}
+                        </a>{' '}
+                        <span className="text-foreground2">{a[2]}</span>
+                     </div>
+                  );
+               })}
+            </section>
+
+            <section className="pt-12">
+               <div className="font-mono text-lg tracking-tight text-foreground2 font-light pb-4">
+                  frontend only projects
+               </div>
+               {[
+                  [
+                     '3d-fizzi.com',
+                     'https://3d-fizzi.vercel.app',
+                     'Landing page for a soda can company',
+                  ],
+                  [
+                     'glisten-ai.com',
+                     'https://modern-glisten-ai.vercel.app',
+                     'Landing page for an AI automation company',
+                  ],
+                  [
+                     '3d-portfolio.com',
+                     'https://p69-three-js-portfolio.vercel.app/',
+                     'A portfolio website with 3D effects',
+                  ],
+                  [
+                     'omnifood.com',
+                     'https://aash417.github.io/p25-Omnifood/',
+                     'Landing page for food ordering service',
+                  ],
+               ].map((a, i) => {
+                  return (
+                     <div key={i + 1}>
+                        <a
+                           href={a[1]}
+                           target="_blank"
+                           className="text-foreground3 hover:text-foreground"
+                        >
+                           {a[0]}
+                        </a>{' '}
+                        <span className="text-foreground2">{a[2]}</span>
+                     </div>
+                  );
+               })}
+            </section>
+
+            <section className="pt-20">
+               <div className="font-mono text-lg tracking-tight text-foreground2 font-light pb-4">
+                  other projects
+               </div>
+               {[
+                  [
+                     'recipe-finder.com',
+                     'https://recipe-finder-aash.netlify.app/',
+                     'Find recipes of your favorite dishes',
+                  ],
+                  [
+                     'roll-a-dice.com',
+                     'https://aash417.github.io/p32-dice-roll/',
+                     'Play a dice rolling game with a friend',
+                  ],
+                  [
+                     'ping-pong.com',
+                     'https://aash417.github.io/p21-pong/',
+                     'Play a ping pong game',
+                  ],
+                  [
+                     'fast-pizza.com',
+                     'https://my-fast-pizza.netlify.app/',
+                     'Order a pizza online',
+                  ],
+                  [
+                     'world-tour.com',
+                     'https://p40-worldtour.netlify.app/',
+                     'Mark your travels on a world map',
+                  ],
+               ].map((a, i) => {
+                  return (
+                     <div key={i + 1}>
+                        <a
+                           href={a[1]}
+                           target="_blank"
+                           className="text-foreground3 hover:text-foreground"
+                        >
+                           {a[0]}
+                        </a>
+                        <span className="text-foreground2">{a[2]}</span>
+                     </div>
+                  );
+               })}
+            </section>
+
+            <footer>
+               <div className="pt-20 text-center text-foreground2 pb-20">
+                  <div className="text-sm">
+                     © {new Date().getFullYear()} aashish
                   </div>
-                );
-              })
-            }
-          </div>
-        </header>
-
-        <section className="pt-4">
-          <div className="text-foreground font-mono text-sm">i am a frontend developer  and a designer that build tools and helps community that empower developers.</div>
-        </section>
-
-
-        <section className="pt-12">
-          <div className="font-mono text-lg tracking-tight text-foreground2 font-light pb-4">
-            packages
-          </div>
-          {
-            [
-              ['check-site-meta', 'https://www.npmjs.com/package/check-site-meta', 'Check site metadata locally via npx before deployment'],
-              ['react-flip-children', 'https://www.npmjs.com/package/react-flip-children', 'FLIP animation for React components'],
-              ['advent-event', 'https://www.npmjs.com/package/advent-event', 'Simple TS library to create advent events'],
-              ['mermaid-ssr', 'https://mermaid-ssr.alfon.dev', 'Server side render mermaid diagrams via http (fetch-only)'],
-              ['nextjs-better-unstable-cache', 'https://www.npmjs.com/package/nextjs-better-unstable-cache', 'Next.js cache function wrapper with better logging and options'],
-            ].map((a, i) => {
-              return (
-                <div key={i}>
-                  <a href={a[1]} target="_blank" className="text-foreground3 hover:text-foreground">{a[0]}</a>{' '}
-                  <span className="text-foreground2">{a[2]}</span>
-                </div>
-              );
-            })
-          }
-        </section>
-
-        <section className="pt-12">
-          <div className="font-mono text-lg tracking-tight text-foreground2 font-light pb-4">
-            directories
-          </div>
-          {
-            [
-              { url: "https://advent-event.alfon.dev", description: "Simple TS library to create advent events." },
-              { url: "https://react-flip-array.alfon.dev", description: "Array reorder animation using FLIP & Code animation" },
-              { url: "https://react-lava.alfon.dev", description: "Learn React via Animation" },
-              { url: "https://dishook.alfon.dev", description: "Discord webhook generator" },
-              { url: "https://advent-of-ui.vercel.app", description: "Advent of UI - 24 days of UI challenges" },
-              { url: "https://nu-auth.vercel.app", description: "Type-safe stateless Next.js Authentication" },
-              { url: "https://vtuberlogos.alfon.dev", description: "VTuber Logo Compilations" },
-              { url: "https://mermaid-ssr.alfon.dev", description: "Server side render mermaid diagrams" },
-
-            ].map((a, i) => {
-              return (
-                <div key={i}>
-                  <a href={a.url} target="_blank" className="text-foreground3 hover:text-foreground">{a.url.replace('https://', '')}</a>{' '}
-                  <span className="text-foreground2">{a.description}</span>
-                </div>
-              );
-            })
-          }
-        </section>
-
-        <section className="pt-20">
-          <div className="font-mono text-lg tracking-tight text-foreground2 font-light pb-4">
-            articles
-          </div>
-          {
-            [
-              { title: "Git Concepts: Crash Course For Beginners", url: "https://alfonsusardani.notion.site/Git-Concepts-Crash-Course-For-Beginners-fa16256c65624048a115e2faa208cf31", description: "alfonsusardani.notion.site - 2024-03-11" },
-              { title: "Next.js Data Cache Behavior", url: "https://alfonsusardani.notion.site/Next-js-Data-Cache-Behavior-44f1a579c6d74f25bc55d301786e94ab", description: "alfonsusardani.notion.site - 2024-02-07" },
-            ].map((a, i) => {
-              return (
-                <div key={i}>
-                  <a href={a.url} target="_blank" className="text-foreground3 hover:text-foreground">{a.title}</a>{' '}
-                  <span className="text-foreground2">{a.description}</span>
-                </div>
-              );
-            })
-          }
-        </section>
-
-        <section className="pt-20">
-          <div className="font-mono text-lg tracking-tight text-foreground2 font-light pb-4">
-            demo
-          </div>
-          {
-            [
-              { url: "https://cautious-palm-tree-ebon.vercel.app", description: "React drag and drop canvas control demo" },
-              { url: "https://art-findr.vercel.app", description: "Demo app to find household assistants (Devscale final assignment)" },
-              { url: "https://devscale-eventmaker-alfonsusac.vercel.app", description: "Devscale 5th assignment" },
-              { url: "https://devscale-alfonsus-assignment-4.vercel.app", description: "Devscale 4th assignment" },
-              { url: "https://assignment-3-devscale.vercel.app/", description: "Devscale 3rd assignment" },
-            ].map((a, i) => {
-              return (
-                <div key={i}>
-                  <a href={a.url} target="_blank" className="text-foreground3 hover:text-foreground">{a.url.replace('https://', '')}</a>{' '}
-                  <span className="text-foreground2">{a.description}</span>
-                </div>
-              );
-            })
-          }
-        </section>
-        <footer>
-          <div className="pt-20 text-center text-foreground2 pb-20">
-            <div className="text-sm">
-              © {new Date().getFullYear()} alfon
-            </div>
-          </div>
-        </footer>
+               </div>
+            </footer>
+         </div>
       </div>
-
-    </div>
-
-  );
+   );
 }
